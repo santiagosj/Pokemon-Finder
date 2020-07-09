73c4ed064e4ccb0e107369324691b6cc307472f6
Santiago Spinetto Jung <sanjs965@gmail.com>
2020-07-09 15:52:32 -0300

    add control flow to card

--- src/Components/PokemonSection/PokemonCard.js
+++ src/Components/PokemonSection/PokemonCard.js
@@ -2,22 +2,30 @@ import React from 'react'
 
 const PokemonCard = ({pokemon}) => {
     return (
-        <div >
-            <h4>id:[{pokemon.id}] - {`Nombre: [${pokemon.name}]`}</h4>
+        
+        <>
+          {pokemon !== null && pokemon !== undefined ? (
+              <div>
+                      <h4>id:[{pokemon.id}] - {`Nombre: [${pokemon.name}]`}</h4>
+                            
+                      <div className={`cardWrapper`}>
             
-            <div className={`cardWrapper`}>
-
-                <div className={`cardItem box`}>
-                    <img className={`cardItem-image`} src={pokemon.sprites.back_default} /> 
-                </div>
-
-                <div className={`cardItem box`}>
-                    <img className={`cardItem-image`} src={pokemon.sprites.front_default} />
-                </div>
+                                <div className={`cardItem box`}>
+                                    <img className={`cardItem-image`} src={pokemon.sprites.back_default} /> 
+                                </div>
+            
+                                <div className={`cardItem box`}>
+                                    <img className={`cardItem-image`} src={pokemon.sprites.front_default} />
+                                </div>
+                                
+                       </div>
+              </div>
                 
-            </div>
-
-        </div>
+          ):(
+              <p>Result</p>
+          )}
+        </>
+        
     )
 }
 
